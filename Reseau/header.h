a672ec405c2b97a8c42cb4b967dc97fa32b99856
Timothée Poux <timothee.poux@gmail.com>
2025-03-24 13:50:28 +0100

    ajout des structs

--- Reseau/header.h
+++ Reseau/header.h
@@ -4,4 +4,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+typedef struct {
+	char id[50];
+	char pseudo[50];
+	char publication[280];
+	struct User* suivant;
+}User;
+
+typedef struct {
+	struct User* premier;
+}Liste;
+
 #endif
